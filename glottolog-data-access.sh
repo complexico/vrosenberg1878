63f2735e73cd3e510536267ae48e76e1e450db63
Gede Rajeg <primahadiwijaya@gmail.com>
2025-04-04 16:02:16 +0800

    Add code to retrieve Glottolog data.

---
+++ glottolog-data-access.sh
@@ -0,0 +1,11 @@
+# accessing glottolog data
+
+## 1. activate virtual environment
+source /Users/Primahadi/Documents/enggano-AHRC/enggano-holle-list-2023-03-27/myenv/bin/activate
+
+## 2. get the languoids data
+glottolog languoids --output=data
+# INFO    glottolog/glottolog at /Users/Primahadi/Documents/cldf_project/glottolog-glottolog-d9da5e2
+# Lagnuoids table written to
+# data/glottolog-languoids-glottolog-glottolog-d9da5e2.csv-metadata.json
+# data/glottolog-languoids-glottolog-glottolog-d9da5e2.csv
